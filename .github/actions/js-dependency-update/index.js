8c9b18f68f934af3b61ba3522bf2c90cfc4eac9f
Narendra Ramakrisha <narendra.raghu@gmail.com>
2025-07-20 18:16:46 -0400

    17-2 JS custom actions

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -3,6 +3,6 @@ import * as core from '@actions/core';
 
 
 async function run() {
-   core.info('I am a custom JS action')
+   core.info('I am a custom JS action checking for dependency updates');
 }
 run();
