d09c5a47b38cdfc719087bcc4dad128d59bd6b3c
Narendra Ramakrisha <narendra.raghu@gmail.com>
2025-08-03 21:21:35 -0400

    Introducing Docker Composite Actions

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,21 +2,6 @@ import os
 import sys
 
 if __name__ == "__main__":
+    print("Hello from Docker Action")
+
     
-
-    # Ensure the script is run with Python 3
-    if sys.version_info[0] < 3:
-        print("This script requires Python 3.")
-        sys.exit(1)
-
-    # Get the URL from environment variable or command line argument
-    url = os.getenv("URL", None)
-    if not url:
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
-        else:
-            print("Please provide a URL to ping.")
-            sys.exit(1)
-
-    # Ping the URL
-    ping_url(url)
